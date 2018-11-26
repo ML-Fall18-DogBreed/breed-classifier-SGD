1d02dd07bfa6fbba82ff4726a713dc495184c660
Lan <lmao@wpi.edu>
2018-11-26 14:14:49 -0500

    a

--- cnn-job.sh
+++ cnn-job.sh
@@ -10,5 +10,5 @@ module load cuda90/blas
 module load cuda90/profiler
 module load cuda90/nsight
 module load cuda90/fft
-source ~/venv/bin/activate
-python classifier.py
+#source ~/venv/bin/activate
+python DogClassifier.py
